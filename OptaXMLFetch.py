36abacaad09e0d78ef2479ec115e3dc8dc483bb4
kuntalbarik <kuntal.barik.11@gmail.com>
2019-11-03 00:57:11 +0530

    Code optimised functionality of processing only c2 and c3 and c50 added 1)c50 code still need to write with proper use case 2)Staus check code need to write 3)Log need to write

--- OptaXMLFetch.py
+++ OptaXMLFetch.py
@@ -1,6 +1,14 @@
 import requests,os,shutil,datetime
 import time
 
+def copyC50FromSource(optaNatchNo):
+   print("c50 are copied")
+
+def renameC50(optaNatchNo):
+   print("c50 are renamed")
+
+def dropC50(optaNatchNo):
+   print("c50 are dropped")
 
 ###check whether C2  & C3 is processed or not
 def checkFile(fileName):
@@ -28,7 +36,7 @@ def waiting_For_processing(fileName,timeStart):
             else:
                time.sleep(60)
                waitingToprocess = time.time()
-               if (waitingToprocess - Cmoved) > 100:
+               if (waitingToprocess - Cmoved) > 420:
                   print("Please check for ",fileName," its not getting processed...")
                   exit(0)
       else:
@@ -36,67 +44,30 @@ def waiting_For_processing(fileName,timeStart):
          i = 1
          return True
 
-def moveC2ToInbox(optaNatchNo):
+def move_C2_C3_ToInbox(fileName):
    ####moving the c2 and c3 file in destination
    moveto = "D:\Watch\Inbox"
    c2c3_filePath="D:\\OptaXML\\XMLs\\"
-   if os.path.isfile(c2c3_filePath + ('crml-{}_C2_.xml'.format(optaMatchNo))):
-      src = c2c3_filePath + ('crml-{}_C2_.xml'.format(optaMatchNo))
-      dst = moveto + '\\' + ('crml-{}_C2_.xml'.format(optaMatchNo))
+   if os.path.isfile(c2c3_filePath + fileName):
+      src = c2c3_filePath + fileName
+      dst = moveto + '\\' + fileName
       shutil.move(src, dst)
-      c2moved = time.time()
-      print('crml-{}_C2_.xml'.format(optaMatchNo)," moved to inbox")
-      return c2moved
+      Cmoved = time.time()
+      print(fileName," moved to inbox")
+      return Cmoved
    else:
-      print('crml-{}_C2_.xml'.format(optaMatchNo), " is not available to drop in inbox")
+      print(fileName, " is not available to drop in inbox")
       a=input("Press Enter to exit")
       exit(0)
 
-def moveC3ToInbox(fileName):
-   c2moved = time.time()
-   moveto = "D:\Watch\Inbox"
-   c2c3_filePath = "D:\\OptaXML\\XMLs\\"
-   if os.path.isfile(c2c3_filePath + ('crml-{}_C3_.xml'.format(optaMatchNo))):
-      src = c2c3_filePath + ('crml-{}_C3_.xml'.format(optaMatchNo))
-      dst = moveto + '\\' + ('crml-{}_C3_.xml'.format(optaMatchNo))
-
-      ###true if c2 processed successfully.If c2 processed moving c3 to inbox
-      check_C2_processState = checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
-      if check_C2_processState == True:
-         shutil.move(src, dst)
-         print('crml-{}_C3_.xml'.format(optaMatchNo), " moved to inbox")
-         c3moved = time.time()
-         return c3moved
-      # If c2 not processed, waiting c2 to get processed and then moving c3 to inbox
-      else:
-         i = 0
-         while (i == 0):
-            print("Waiting for " + 'crml-{}_C2_.xml'.format(optaMatchNo) + " to get processed")
-            check_C2_processState = checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
-            ###if c2 processed and is in success folder, moving c3 to inbox
-            if check_C2_processState == True:
-               shutil.move(src, dst)
-               print('crml-{}_C3_.xml'.format(optaMatchNo), " moved to inbox")
-               c3moved = time.time()
-               return c3moved
-            else:
-               time.sleep(60)
-               waitingForC2Toprocess = time.time()
-               if (waitingForC2Toprocess - c2moved) > 100:
-                  print('crml-{}_C3_.xml'.format(optaMatchNo), " is not available to drop in inbox")
-                  a = input("Press Enter to exit")
-                  exit(0)
-
 def generateC2C3(optaMatchNo):
-   c2Url = 'http://omo.akamai.opta.net/?game_id={}&feed_type=c2&user=PrimeFocus&psw=eZPW4bhFYtX'.format(optaMatchNo)
-   c3Url = 'http://omo.akamai.opta.net/?game_id={}&feed_type=c3&user=PrimeFocus&psw=eZPW4bhFYtX'.format(optaMatchNo)
    c2c3_filePath = "D:\\OptaXML\\XMLs\\"
-   ###Creating C2 file
+   c2Url = 'http://omo.akamai.opta.net/?game_id={}&feed_type=c2&user=PrimeFocus&psw=eZPW4bhFYtX'.format(optaMatchNo)
    resp = requests.get(r'' + c2Url)
    with open(c2c3_filePath + 'crml-' + optaMatchNo + '_C2_.xml', 'wb') as foutput:
       foutput.write(resp.content)
 
-   ###Creating C3 file
+   c3Url = 'http://omo.akamai.opta.net/?game_id={}&feed_type=c3&user=PrimeFocus&psw=eZPW4bhFYtX'.format(optaMatchNo)
    resp = requests.get(r'' + c3Url)
    with open(c2c3_filePath + 'crml-' + optaMatchNo + '_C3_.xml', 'wb') as foutput:
       foutput.write(resp.content)
@@ -123,53 +94,88 @@ def generateC2C3(optaMatchNo):
             file.write(fileData)
 
 def processAll(optaMatchNo):
-   c2moved=moveC2ToInbox(optaMatchNo)
-   isC2Processed=checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
+   c2moved=move_C2_C3_ToInbox('crml-{}_C2_.xml'.format(optaMatchNo))
    i=0
    while i==0:
       j=waiting_For_processing('crml-{}_C2_.xml'.format(optaMatchNo),c2moved)
       if(j==True):
          i=1
    ###if C2 processed moving C3 to inbox
-   c3moved = moveC3ToInbox(optaMatchNo)
-   isC3Processed = checkFile('crml-{}_C3_.xml'.format(optaMatchNo))
+   c3moved = move_C2_C3_ToInbox('crml-{}_C3_.xml'.format(optaMatchNo))
    i = 0
    while i == 0:
       j = waiting_For_processing('crml-{}_C3_.xml'.format(optaMatchNo), c3moved)
       if (j == True):
          i = 1
+   ####processing c50
+   copyC50FromSource(optaMatchNo)
+   renameC50(optaMatchNo)
+   dropC50(optaMatchNo)
 
 option=int(input("Please select processing steps :-\n1 => To process All(C2,C3,C50)\n2 => To process only C2\n"
       "3 => To process only C3\n4 => To process C50\n5 => To check status\n6 => To exit\n").strip())
 
 if(option in (1,2,3)):
    optaMatchNo = input("Please enter opta match id : ").strip()
-   ###generating c2 and c3
-   generateC2C3(optaMatchNo)
 
    if(option==1):
+      generateC2C3(optaMatchNo)
       processAll(optaMatchNo)
 
    elif(option==2):
-      c2moved = moveC2ToInbox(optaMatchNo)
-      isC2Processed = checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
-      i = 0
-      while i == 0:
-         j = waiting_For_processing('crml-{}_C2_.xml'.format(optaMatchNo), c2moved)
-         if (j == True):
-            i = 1
+      isC2Processed=checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
+      if isC2Processed==False:
+         if os.path.isfile("D:\\OptaXML\\XMLs\\" + 'crml-{}_C2_.xml'.format(optaMatchNo)):
+            c2moved = move_C2_C3_ToInbox('crml-{}_C2_.xml'.format(optaMatchNo))
+            i = 0
+            while i == 0:
+               j = waiting_For_processing('crml-{}_C2_.xml'.format(optaMatchNo), c2moved)
+               if (j == True):
+                  i = 1
+         else:
+            print('crml-{}_C2_.xml'.format(optaMatchNo), " is not in source to drop")
+      else:
+         print('crml-{}_C2_.xml'.format(optaMatchNo)," is already processed,available in success folder")
+   ###processing C3
    else:
-      c3moved = moveC3ToInbox(optaMatchNo)
-      isC3Processed = checkFile('crml-{}_C3_.xml'.format(optaMatchNo))
-      i = 0
-      while i == 0:
-         j = waiting_For_processing('crml-{}_C3_.xml'.format(optaMatchNo), c3moved)
-         if (j == True):
-            i = 1
+      ##checking whether c2 processed or not
+      isC2Processed = checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
+      if isC2Processed==True:
+         isC3Processed = checkFile('crml-{}_C3_.xml'.format(optaMatchNo))
+         if isC3Processed == False:
+            if os.path.isfile("D:\\OptaXML\\XMLs\\" + 'crml-{}_C3_.xml'.format(optaMatchNo)):
+               c3moved = move_C2_C3_ToInbox('crml-{}_C3_.xml'.format(optaMatchNo))
+               i = 0
+               while i == 0:
+                  j = waiting_For_processing('crml-{}_C3_.xml'.format(optaMatchNo), c3moved)
+                  if (j == True):
+                     i = 1
+            else:
+               print('crml-{}_C3_.xml'.format(optaMatchNo), " is not in source to drop")
+         else:
+            print('crml-{}_C3_.xml'.format(optaMatchNo), " is already processed,available in success folder")
+      else:
+         print('crml-{}_C2_.xml'.format(optaMatchNo)," is not processed yet,Please process ",
+               'crml-{}_C2_.xml'.format(optaMatchNo)," first")
 
 elif(option==4):
    optaMatchNo = input("Please enter opta match id : ").strip()
-   print("processing c50")
+   isC2Processed = checkFile('crml-{}_C2_.xml'.format(optaMatchNo))
+   if isC2Processed==True:
+      isC3Processed = checkFile('crml-{}_C3_.xml'.format(optaMatchNo))
+      if isC3Processed==True:
+         print("processing c50 can start")
+         copyC50FromSource(optaMatchNo)
+         renameC50(optaMatchNo)
+         dropC50(optaMatchNo)
+
+      else:
+         print('crml-{}_C3_.xml'.format(optaMatchNo), " is not processed yet,Please process ",
+               'crml-{}_C3_.xml'.format(optaMatchNo), " first")
+
+   else:
+      print('crml-{}_C2_.xml'.format(optaMatchNo), " is not processed yet,Please process ",
+            'crml-{}_C2_.xml'.format(optaMatchNo), " first")
 
 elif(option==5):
    optaMatchNo = input("Please enter opta match id : ").strip()
